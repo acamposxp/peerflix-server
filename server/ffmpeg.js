73a914a7415b8e6158df007c37e5f753dc9fe627
acamposxp <acamposxp@mail.ru>
2016-05-22 23:09:15 -0300

    Update ffmpeg.js

--- server/ffmpeg.js
+++ server/ffmpeg.js
@@ -25,9 +25,9 @@ module.exports = function (req, res, torrent, file) {
   }
 
   function remux() {
-    res.type('video/webm');
+    res.type('video/mp4');
     var command = ffmpeg(file.createReadStream())
-      .videoCodec('libvpx').audioCodec('libvorbis').format('webm')
+      .videoCodec('libx264').audioCodec('libmp3lame').format('mp4')
       .audioBitrate(128)
       .videoBitrate(1024)
       .outputOptions([
